58582f19d5fb0181306e67f94edee4679ee99e7e
Daniel Vintila <c00307041@setu.ie>
2025-09-15 14:48:04 +0000

    Finished Lab1

--- Lab1.py
+++ Lab1.py
@@ -1,4 +1,17 @@
-print("Hello World") 
-print("New update")
+'''Lab 1 
+intro to python and GitHub
+'''
 
+print("How old are you?")               #Prints the question
+age = input()                           #age - variable which takes data from keyboard
+print("you are", age , "years old")     #Prints message + variable
+
+name = input("What is your name?\t")
+print("Your name is ", name)
+
+print("Enter 2 numbers below:\t")
+number1 = int(input())
+number2 = int(input())
+answer = number1+number2
+print(number1, "+", number2, " =", answer)
 
